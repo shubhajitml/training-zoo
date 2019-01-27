5e1ecf6d0eb394dd839ccfe1d295f6c20574a1e4
Shubhajitdas121 <shubhajitdas121@gmail.com>
2019-01-28 00:34:24 +0530

    refactored

---
+++ resnet/resnet_models.py
@@ -0,0 +1,192 @@
+# the paper: [Deep Residual Learning for Image Recognition](https://arxiv.org/abs/1512.03385)
+# code reference: [Pytorch](https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py)
+
+
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+
+__all__ = ['Resnet', 'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152']
+
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
+
+def conv3x3(in_channels, out_channels, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+
+def conv1x1(in_channels, out_channels, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False)
+
+class BasicBlock(nn.Module):
+    """ BasicBlock (identity shortcuts) for resnet18 and resnet34 """
+    block_expansion = 1
+
+    def __init__(self, in_channels, channels, stride=1, downsample=None):
+        super().__init__()
+        self.conv1 = conv3x3(in_channels, channels, stride)
+        self.bn1 = nn.BatchNorm2d(channels)
+        self.conv2 = conv3x3(channels, channels)
+        self.bn2 = nn.BatchNorm2d(channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.stride = stride
+        self.downsample = downsample
+
+    def forward(self,x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None: identity = self.downsample(x)
+        out += identity
+
+        out = self.relu(out)
+        return out
+
+class Bottleneck(nn.Module):
+    """ Bottleneck (projection shortcuts) for resnet18 and resnet34 """
+    block_expansion = 4
+
+    def __init__(self, in_channels, channels, stride=1, downsample=None):
+        super().__init__()
+        self.conv1 = conv1x1(in_channels, channels)
+        self.bn1 = nn.BatchNorm2d(channels)
+        self.conv2 = conv3x3(channels, channels, stride)
+        self.bn2 = nn.BatchNorm2d(channels)
+        self.conv3 = conv1x1(channels, channels * self.block_expansion)
+        self.bn3 = nn.BatchNorm2d(channels * self.block_expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.stride = stride
+        self.downsample = downsample
+    
+    def forward(self, x):
+        identity = x
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+        
+        if self.downsample is not None: identity = self.downsample(x)
+        out += identity
+
+        out = self.relu(out)
+        return out
+
+class Resnet(nn.Module):
+    def __init__(self, block, layers, num_classes=1000):
+        super().__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.conv2x_layer = self._stack_blocks(block, 64, layers[0])
+        self.conv3x_layer = self._stack_blocks(block, 128, layers[1], stride=2)
+        self.conv4x_layer = self._stack_blocks(block, 256, layers[2], stride=2)
+        self.conv5x_layer = self._stack_blocks(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.fc = nn.Linear(512 * block.block_expansion, num_classes)
+
+    def _stack_blocks(self, block, channels, num_blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.in_channels != channels * block.block_expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.in_channels, channels * block.block_expansion, stride),
+                nn.BatchNorm2d(channels * block.block_expansion)
+            )
+        layers = []
+        layers.append(block(self.in_channels, channels, stride, downsample))
+        self.in_channels = channels * block.block_expansion
+        for _ in (1, num_blocks):
+            layers.append(block(self.in_channels, channels))
+        
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.maxpool(out)
+
+        out = self.conv2x_layer(out)
+        out = self.conv3x_layer(out)
+        out = self.conv4x_layer(out)
+        out = self.conv5x_layer(out)
+
+        out = self.avgpool(out)
+        out = out.view(out.size(0), -1) # reshaping dimension to (dim,1) from (dim,1,1)
+        out = self.fc(out)
+        
+        return out
+
+def resnet18(pretrained=False, **kwargs):
+    """Constructs a ResNet-18 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = Resnet(block=BasicBlock, layers=[2, 2, 2, 2], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
+    return model
+
+def resnet34(pretrained=False, **kwargs):
+    """Constructs a ResNet-34 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = Resnet(block=BasicBlock, layers=[3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet34']))
+    return model
+
+def resnet50(pretrained=False, **kwargs):
+    """Constructs a ResNet-50 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = Resnet(block=Bottleneck, layers=[3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
+    return model
+
+def resnet101(pretrained=False, **kwargs):
+    """Constructs a ResNet-101 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = Resnet(block=Bottleneck, layers=[3, 4, 23, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet101']))
+    return model
+
+def resnet152(pretrained=False, **kwargs):
+    """Constructs a ResNet-152 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = Resnet(block=Bottleneck, layers=[3, 8, 36, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet152']))
+    return model
